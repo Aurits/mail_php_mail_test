84113355312ff2c0a9ff9b93e2e7a4f50b23e816
Ambrose Alanda <alandaambrose@gmail.com>
2023-11-14 18:45:41 +0000

    Nov 14, 2023, 9:45 PM

---
+++ resources/views/livewire/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your App Title</title>
+    <!-- Include any additional stylesheets or scripts -->
+</head>
+
+<body>
+    @yield('content')
+    <!-- This is where Livewire components will be rendered -->
+</body>
+
+</html>
