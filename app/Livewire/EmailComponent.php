6a7794a86888191bde84afe073d7ade7f93aabad
Ambrose Alanda <alandaambrose@gmail.com>
2023-11-14 16:20:48 +0000

    Nov 14, 2023, 7:20 PM

--- app/Livewire/EmailComponent.php
+++ app/Livewire/EmailComponent.php
@@ -16,42 +16,43 @@ class EmailComponent extends Component
         $this->fetchEmails();
     }
 
+    private function fetchEmails()
+    {
+        try {
+            $mail = new PHPMailer(true);
 
+            // Configure PHPMailer settings for IMAP with STARTTLS
+            $mail->isIMAP();
+            $mail->Host = 'webmail.mak.ac.ug';
+            $mail->Port = 993;
+            $mail->SMTPSecure = 'starttls'; // STARTTLS
+            $mail->SMTPAuth = true;
+            $mail->Username = config('mail.imap_username'); // Use env variables or config
+            $mail->Password = config('mail.imap_password'); // Use env variables or config
 
-private function fetchEmails()
-{
-    try {
-        $mail = new PHPMailer(true);
+            // Connect to the server
+            $mail->connect();
 
-        // Configure PHPMailer settings for IMAP with STARTTLS
-        $mail->isIMAP();
-        $mail->Host = 'webmail.mak.ac.ug';
-        $mail->Port = 993;
-        $mail->SMTPSecure = 'tls'; // STARTTLS
-        $mail->SMTPAuth = true;
-        $mail->Username = 'your-email@example.com';
-        $mail->Password = 'your-email-password';
+            // Select the mailbox
+            $mail->select('INBOX');
 
-        // Connect to the server
-        $mail->connect();
+            // Search for all emails
+            $emails = $mail->searchMailbox('ALL');
 
-        // Select the mailbox
-        $mail->select('INBOX');
-
-        // Search for all emails
-        $emails = $mail->searchMailbox('ALL');
-
-        // Fetch email subjects (you can customize this based on your needs)
-        $this->emails = array_map(function ($email) {
-            return $email->subject;
-        }, $emails);
-
-    } catch (Exception $e) {
-        // Handle exceptions
-        $this->addError('fetchEmails', 'Error fetching emails: ' . $e->getMessage());
+            // Fetch email subjects (you can customize this based on your needs)
+            $this->emails = array_map(function ($email) {
+                return $email->subject;
+            }, $emails);
+        } catch (Exception $e) {
+            // Handle exceptions
+            $this->addError('fetchEmails', 'Error fetching emails: ' . $e->getMessage());
+        } finally {
+            // Close the connection
+            if (isset($mail)) {
+                $mail->close();
+            }
+        }
     }
-}
-
 
     public function render()
     {
