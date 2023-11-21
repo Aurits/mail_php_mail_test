c5edcf5dd97a1269251c60ad82e8ccde4e251353
Ambrose Alanda <alandaambrose@gmail.com>
2023-11-21 16:51:30 +0000

    Nov 21, 2023, 7:51 PM

--- app/Api/EmailApiController.php
+++ app/Api/EmailApiController.php
@@ -12,7 +12,7 @@ class EmailApiController extends Controller
     {
         try {
             // Connect to the IMAP server
-            $mailbox = imap_open("{webmail.mak.ac.ug:993/imap/ssl}", 'alandaambrose@students.mak.ac.ug', 'Gloria11111.@');
+            $mailbox = imap_open("{webmail.mak.ac.ug:993/imap/ssl}INBOX", 'ambrose.alanda@students.mak.ac.ug', 'Gloria11111.@');
 
             if ($mailbox) {
                 // Fetch emails
