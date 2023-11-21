f2aa7218b23a47546ffaa0161eeb87f611f95cb9
Ambrose Alanda <alandaambrose@gmail.com>
2023-11-21 14:36:58 +0000

    Nov 21, 2023, 5:36 PM

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Livewire\EmailApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-use App\Http\Controllers\Api\EmailApiController;
 
 Route::get('/fetch-emails', [EmailApiController::class, 'fetchEmails']);
