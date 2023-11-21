3080a3d6c91c20fac904e6b6805c1d7c7e157b4c
Ambrose Alanda <alandaambrose@gmail.com>
2023-11-21 14:56:01 +0000

    Nov 21, 2023, 5:56 PM

--- app/Api/EmailApiController.php
+++ app/Api/EmailApiController.php
@@ -26,8 +26,13 @@ class EmailApiController extends Controller
 
                         // Add email details to the array
                         $emailData[] = [
+                            'from' => imap_headerinfo($mailbox, $emailId)->fromaddress,
+                            'to' => imap_headerinfo($mailbox, $emailId)->toaddress,
+                            'reply_to' => imap_headerinfo($mailbox, $emailId)->reply_toaddress,
+                            'date' => date('Y-m-d H:i:s', strtotime(imap_headerinfo($mailbox, $emailId)->date)),
                             'subject' => imap_headerinfo($mailbox, $emailId)->subject,
-                            // 'message' => imap_body($mailbox, $emailId),
+                            'message' => $this->getBody($mailbox, $emailId, $emailDetails),
+                            'attachments' => $this->getAttachments($mailbox, $emailId, $emailDetails),
                             // Add other email details as needed
                         ];
                     }
@@ -46,5 +51,54 @@ class EmailApiController extends Controller
             // Handle exceptions
             return response()->json(['error' => 'Error fetching emails: ' . $e->getMessage()], 500);
         }
+    }
+
+    private function getBody($mailbox, $emailId, $emailDetails)
+    {
+        // Initialize the body variable
+        $body = '';
+
+        // Check if the email has multiple parts (MIME)
+        if ($emailDetails->type === 1) {
+            // Fetch the HTML and plain text parts if available
+            $htmlPart = imap_fetchbody($mailbox, $emailId, '1.1');
+            $plainPart = imap_fetchbody($mailbox, $emailId, '1.2');
+
+            // Prioritize HTML over plain text
+            $body = !empty($htmlPart) ? $htmlPart : $plainPart;
+        } else {
+            // Fetch the body for non-MIME emails
+            $body = imap_body($mailbox, $emailId);
+        }
+
+        // Remove unwanted characters or formatting if needed
+
+        return $body;
+    }
+
+    private function getAttachments($mailbox, $emailId, $emailDetails)
+    {
+        // Initialize the attachments array
+        $attachments = [];
+
+        // Check if the email has multiple parts (MIME)
+        if ($emailDetails->type === 1) {
+            // Loop through each part of the email
+            foreach ($emailDetails->parts as $partId => $part) {
+                // Check if the part has a filename (indicating an attachment)
+                if (isset($part->disposition) && strtoupper($part->disposition) === 'ATTACHMENT') {
+                    // Fetch the attachment
+                    $attachment = [
+                        'filename' => $part->dparameters[0]->value,
+                        'content' => imap_fetchbody($mailbox, $emailId, $partId + 1),
+                    ];
+
+                    // Add the attachment to the attachments array
+                    $attachments[] = $attachment;
+                }
+            }
+        }
+
+        return $attachments;
     }
 }
