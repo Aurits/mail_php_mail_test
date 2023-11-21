a58f7d9544f5b65519e7df596bf70ccc7cd5fa4e
Ambrose Alanda <alandaambrose@gmail.com>
2023-11-21 14:40:22 +0000

    Nov 21, 2023, 5:40 PM

--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::get('/fetch-emails', [EmailApiController::class, 'fetchEmails']);
+// Route::get('/fetch-emails', [EmailApiController::class, 'fetchEmails']);
