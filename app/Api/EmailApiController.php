ba8cf1ac317285a22b6aa989a5e22ee87d421253
Ambrose Alanda <alandaambrose@gmail.com>
2023-11-22 05:51:31 +0000

    Nov 22, 2023, 8:51 AM

--- app/Api/EmailApiController.php
+++ app/Api/EmailApiController.php
@@ -144,7 +144,7 @@ class EmailApiController extends Controller
                     if ($partNumber) {
                         $prefix = $partNumber . ".";
                     }
-                    $data = $this->get_part($mailbox, $uid, $mimetype, $subStruct, $prefix . ($index));
+                    $data = $this->get_part($mailbox, $uid, $mimetype, $subStruct, $prefix . ($index - 1));
                     if ($data) {
                         return $data;
                     }
