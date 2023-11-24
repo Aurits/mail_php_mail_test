639dc38890aa34ed24ed67bf204ffdb3dece55cc
Ambrose Alanda <alandaambrose@gmail.com>
2023-11-24 10:11:56 +0000

    Nov 24, 2023, 1:11 PM

--- app/Livewire/EmailComponent.php
+++ app/Livewire/EmailComponent.php
@@ -20,7 +20,7 @@ class EmailComponent extends Component
     {
         try {
             // Connect to the IMAP server
-            $mailbox = imap_open("{webmail.mak.ac.ug:993/imap/ssl}INBOX", 'ambrose.alanda@students.mak.ac.ug', 'Gloria11111.@');
+            $mailbox = imap_open("{webmail.mak.ac.ug:993/imap/ssl}", 'ambrose.alanda@students.mak.ac.ug', 'Gloria11111.@');
 
             if ($mailbox) {
                 // Fetch emails
